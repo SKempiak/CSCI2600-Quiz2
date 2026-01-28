6dc79a7fedc5eeea2bea734143a991dc5fd19871
SKempiak <kempis@rpi.edu>
2026-01-28 18:53:59 -0500

    added random numbers

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,11 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("Generating random numbers:");
+        for (int i = 0; i < 5; i++) {
+            int randomNum = (int)(Math.random() * 100);
+            System.out.println("Random Value " + (i + 1) + ": " + randomNum);
+        }
+        System.out.println("Done.");
     }
 }
-
