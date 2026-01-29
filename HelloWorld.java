2687f610d2c215cbed81c3d840875b80e1d872da
Samson Kempiak <kempis@rpi.edu>
2026-01-29 00:46:16 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("Generating random numbers:");
-        for (int i = 0; i < 5; i++) {
-            int randomNum = (int)(Math.random() * 100);
-            System.out.println("Random Value " + (i + 1) + ": " + randomNum);
+        System.out.println("Counting to 10:");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
         }
-        System.out.println("Done.");
     }
 }
